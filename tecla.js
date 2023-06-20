30dc985cb664eab72a80af2548074da08e2fad5d
Rafael Lindemann Duarte <69912539+rafaellindemann@users.noreply.github.com>
2023-06-20 13:51:24 -0300

    Update tecla.js
    Generalização do script para atender teclas numéricas mas sem estar amarrado a um html específico.
    Indicações sobre como tratar teclas não numéricas.

--- tecla.js
+++ tecla.js
@@ -1,11 +1,11 @@
+// isto serve para tratar as teclas numéricas.
+// para outras teclas, remover a conversão para Number
 document.onkeyup = function (evento){
     let tecla = Number(evento.key)
-    document.getElementById('divNumero').innerHTML = tecla
-    // document.getElementById('inpNumero').value = tecla
-    // console.log(!isNaN(tecla))
+    console.log(!isNaN(tecla))
+    console.log(tecla)
     if(!isNaN(tecla))
-    {
-        // aqui eu sei que é uma resposta e posso corrigir
-    }
-    // trampo pra fazer com aquele valor da tecla
+    {// aqui eu sei que é uma resposta e posso corrigir
+        console.log("Número")
+    }  
 }
